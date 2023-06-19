e6f582a772aa09af602fd45d9ca7e43dbbdaf1c2
kiqmachado <127623080+kiqmachado@users.noreply.github.com>
2023-06-18 21:27:08 -0300

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,1 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider,  } from 'firebase/auth';
+import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {}; // apague esta linha inteira e cole a sua firebaseConfig.
+
+const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
