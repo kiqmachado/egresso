3a8195eed2dc9dbf3afec4eece318fa42146523f
kiqmachado <127623080+kiqmachado@users.noreply.github.com>
2023-07-02 13:34:22 -0300

    ultimo update 2.0

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.unstable_createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
@@ -12,3 +12,4 @@ root.render(
 );
 
 reportWebVitals();
+
